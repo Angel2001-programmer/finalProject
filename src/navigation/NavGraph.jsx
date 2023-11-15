499fe6b4be2e9ee67a8aa1b7ac54b0424eccaac5
Angel2001-programmer <angelwitchell2001@gmail.com>
2023-11-15 18:37:49 +0000

    Error Page implemented.

--- src/navigation/NavGraph.jsx
+++ src/navigation/NavGraph.jsx
@@ -1,6 +1,7 @@
 import Home from '../pages/home/home';
 import About from '../pages/about/about';
 import Contact from '../pages/contact/contact';
+import Error from '../pages/error/error';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 const NavGraph = () => {
@@ -13,6 +14,7 @@ const NavGraph = () => {
             element={<About/>}/>
             <Route path="/contact"
             element={<Contact/>}/>
+            <Route path='*' element={<Error/>}/>
         </Routes>
         </BrowserRouter>
        
