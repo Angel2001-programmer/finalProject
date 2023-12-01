93049eb7ca39a6877a1c28cfab38d6597d2e62bc
Angel2001-programmer <angelwitchell2001@gmail.com>
2023-12-01 11:11:26 +0000

    fixed routing

--- src/navigation/NavGraph.jsx
+++ src/navigation/NavGraph.jsx
@@ -10,8 +10,7 @@ const NavGraph = () => {
          <Routes>
             <Route path="/home"
             element={<Home/>}/>
-            <Route path="/finalProject"
-            element={<About/>}/>
+            <Route path="/" element={<About/>}/>
             <Route path="/contact"
             element={<Contact/>}/>
             <Route path='*' element={<Error/>}/>
