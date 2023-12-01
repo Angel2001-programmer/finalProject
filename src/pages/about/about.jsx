5a8a91889f03946b5f51cbb2908c9ba5a3f2e76a
Meteorstrike <k.h.materials@gmail.com>
2023-12-01 12:24:10 +0000

    Formatting

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -20,46 +20,46 @@ export default function About() {
 					<p className='mainText'>About</p>
 					<h3>Team Introductiom</h3>
 				<div className={styles.about}>
-						<TeamMemeber
-							name='Angel'
-							profilepictrue={Angel}
-							hobby='Coding websites in my spare time.'
-							purpose='To get a job and have a good life'
-						/>
+          <TeamMemeber
+            name='Angel'
+            profilepictrue={Angel}
+            hobby='Coding websites in my spare time.'
+            purpose='To get a job and have a good life'
+          />
 
-						<TeamMemeber
-							name='Your Name'
-							// if you want your image on the website place it into the img folder,
-							// and link it same as me with import on the top of the file.
-							profilepictrue=''
-							hobby='Your hobby/activity'
-							purpose='Why do you do it?'
-						/>
+          <TeamMemeber
+            name='Your Name'
+            // if you want your image on the website place it into the img folder,
+            // and link it same as me with import on the top of the file.
+            profilepictrue=''
+            hobby='Your hobby/activity'
+            purpose='Why do you do it?'
+          />
 
-      <TeamMemeber 
-      name='Abbie' 
-      // if you want your image on the website place it into the img folder, 
-      // and link it same as me with import on the top of the file.
-      profilepictrue='' 
-      hobby='Reading' 
-      purpose='For mental stimulation and relaxation'
-      />
+          <TeamMemeber 
+          name='Abbie' 
+          // if you want your image on the website place it into the img folder, 
+          // and link it same as me with import on the top of the file.
+          profilepictrue='' 
+          hobby='Reading' 
+          purpose='For mental stimulation and relaxation'
+          />
 
-      <TeamMemeber 
-      name='Katherine Hooper' 
-      // if you want your image on the website place it into the img folder, 
-      // and link it same as me with import on the top of the file.
-      profilepictrue={Katherine} 
-      hobby='I enjoy running and kickboxing' 
-      purpose='Running feels freeing and helps me think. Kickboxing makes me feel connected and in control. I love to challenge myself and work hard to progress and improve.'
-      />
-      
-			<TeamMemeber
-			name='Haiying Liao'
-			profilepictrue={haiying}
-			hobby='I like to keep learning new stuff'
-			purpose='because I enjoy leaning :)' />  
-      </div>
+          <TeamMemeber 
+          name='Katherine Hooper' 
+          // if you want your image on the website place it into the img folder, 
+          // and link it same as me with import on the top of the file.
+          profilepictrue={Katherine} 
+          hobby='I enjoy running and kickboxing' 
+          purpose='Running feels freeing and helps me think. Kickboxing makes me feel connected and in control. I love to challenge myself and work hard to progress and improve.'
+          />
+          
+          <TeamMemeber
+          name='Haiying Liao'
+          profilepictrue={haiying}
+          hobby='I like to keep learning new stuff'
+          purpose='because I enjoy leaning :)' />  
+        </div>
       </section>
     </main>
     <footer className="footer"><p>(Group 1 Name) all rights reserved</p></footer>
