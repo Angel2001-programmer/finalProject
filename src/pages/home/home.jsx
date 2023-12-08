051495a58275c3c122ac03edcbba3be5b4dbc43e
Angel2001-programmer <angelwitchell2001@gmail.com>
2023-12-08 20:16:17 +0000

    HomePage styled.

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,21 +2,34 @@ import { Fragment } from 'react'
 import NavBar from '../../components/NavBar/navbar';
 import styles from "./home.module.css";
 import HomeItem from "../../components/HomeItem/HomeItem";
+import Community from "../../assets/images/community.png"
+import Gaming from "../../assets/images/gaming.png"
+import Books from "../../assets/images/books.png"
+import Friendship from "../../assets/images/friendship.png"
+import TreatOthers from "../../assets/images/treatothers.png"
+import News from "../../assets/images/discussions.png"
+import dropArrow from "../../assets/images/drop_Icon.svg"
 export default function Home() {  
   return (
     <Fragment>
     <NavBar />
     <main className={styles.main}>
+      <div className={styles.rowHome}>
+      <div className={styles.containerHeading}>
+      <h1 className={styles.textHeading}>Find your community</h1>
+      </div>
+      <img className={styles.dropArrow} src={dropArrow} alt='Scroll down for more information'/>
+      </div>
     </main>
     <div className={styles.container0}>
-      <HomeItem/>
-      <HomeItem/>
-      <HomeItem/>
+    <HomeItem text="A friendly place for introverts and everyone to express themseleves in safe a good environment" image={Community}/>
+      <HomeItem text="A place for gamers of all kinds ranging from PC, Xbox, Playstation and much more!" image={Gaming}/>
+      <HomeItem text="A place for book readers, to discuss there most loved pages." image={Books}/>
     </div>
     <div className={styles.container1}>
-      <HomeItem/>
-      <HomeItem/>
-      <HomeItem/>
+      <HomeItem text="This is a place where you can find guidance and support if you are having a tough time," image={Friendship}/>
+      <HomeItem text="All we ask is you treat users on this website with respect and how you wish to be treated!" image={TreatOthers}/>
+      <HomeItem text="We have plenty of anime discussion and news. you won’t want to miss this!" image={News}/>
     </div>
     </Fragment>
   )
