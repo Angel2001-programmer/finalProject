7b3940f3720b996966923d3812bdefe0cea3b227
Meteorstrike <k.h.materials@gmail.com>
2023-12-11 16:57:50 +0000

    Modifying register user function

--- flask-server/app.py
+++ flask-server/app.py
@@ -3,7 +3,8 @@ from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_session import Session  # Need to find something to replace this
 from config import ApplicationConfig
-from models.user_models import db, User
+from models.user_models import db, User, Profile
+from models.content_models import db, Books, Anime, Games
 
 # This file might get quite big, might need to have some functions elsewhere and call them here instead
 
@@ -15,6 +16,7 @@ cors = CORS(app, supports_credentials=True)
 # server_session = Session(app) # By default session would be client side without this server session, but since enabled it will always be stored on server side
 db.init_app(app)
 
+# Will create all tables
 with app.app_context():
     db.create_all()
 
@@ -25,26 +27,29 @@ def home():
 
 # Register
 @app.route("/register", methods=["POST"])
-# def register_user():
-#     email = request.json["email"]  # Gets it from the json
-#     password = request.json["password"]
+def register_user():
+    username = request.json["username"]  # Gets it from the json
+    first_name = request.json["first_name"]
+    last_name = request.json["last_name"]
+    email = request.json["email"]
+    password = request.json["password"]
     
-#     user_exists = User.query.filter_by(email=email).first() is not None
+    user_exists = User.query.filter_by(username=username).first() is not None
 
-#     if user_exists:
-#         return jsonify({"error": "Email is already registered"}), 409
+    if user_exists:
+        return jsonify({"error": "Email is already registered"}), 409
 
-#     hashed_password = bcrypt.generate_password_hash(password)
-#     new_user = User(email=email, password=hashed_password)  # Creating an instance of User class, id is default
-#     db.session.add(new_user)
-#     db.session.commit()
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_user = User(email=email, password=hashed_password)  # Creating an instance of User class, id is default
+    db.session.add(new_user)
+    db.session.commit()
 
-#     session["user_id"] = new_user.id  # Doesn't work with flask 3.0
+    session["user_id"] = new_user.id  # Doesn't work with flask 3.0
 
-#     return jsonify({
-#         "id": new_user.id,
-#         "email": new_user.email
-#     })
+    return jsonify({
+        "id": new_user.id,
+        "email": new_user.email
+    })
 
 # Login
 @app.route("/login", methods=["POST"])
