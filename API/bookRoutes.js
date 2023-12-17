72e78eed916ffd50dc87921a095791ddae03966f
Ahbe13 <132223967+Ahbe13@users.noreply.github.com>
2023-12-17 21:45:46 +0000

    Update bookRoutes.js

--- API/bookRoutes.js
+++ API/bookRoutes.js
@@ -1,23 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const connection = require('./db');
-const { handleDatabaseErrors } = require('./errorHandling');
+const db = require('./db');
+const suggestionsData = require('./suggestionsData');
 
-router.get('/', (req, res) => {
-    console.log('Fetching books from the database...');
-    connection.query('SELECT * FROM Books', (err, results) => {
+router.get('/:genre', (req, res) => {
+    const { genre } = req.params;
+    const tablename = 'Books'; 
+    const genreColumn = `${tablename}_genre`; 
+
+    const query = `SELECT Book_Name, Book_Author, Book_Genre, Price, Book_Script FROM ${tablename} WHERE ${genreColumn} = ?`;
+
+    db.query(query, [genre], (err, results) => {
         if (err) {
-            console.error('Error querying MySQL - Books: Please check your query.', err);
-            res.status(500).json({ error: 'Error querying MySQL Books' });
+            console.error(`Error fetching ${tablename} suggestions for ${genre}:`, err);
+            res.status(500).json({ error: `Error fetching ${tablename} suggestions for ${genre}` });
             return;
         }
-        if (results.length === 0) {
-            console.log('No books found in the database.');
-        } else {
-            console.log('Books fetched successfully.');
-        }
-        res.json(results);
+
+        const mergedResults = results.map(result => {
+            const matchingSuggestion = suggestionsData[tablename][genre].find(suggestion => suggestion.title === result['Book_Name']);
+
+            if (matchingSuggestion) {
+                return {
+                    ...result,
+                    image: matchingSuggestion.image
+                };
+            }
+            return result;
+        });
+
+        res.json(mergedResults);
     });
 });
 
-module.exports = { router, connection, handleDatabaseErrors };
+module.exports = router;
